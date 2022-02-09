741936db40c2aaf1356969ffea7b3a1820e81517
gazeeb <gazeebp007@gmail.com>
2022-02-09 23:26:19 +0530

    node project moularisation day-1 date:9/2/22 commint no 2

--- apps.js
+++ apps.js
@@ -1,22 +1,14 @@
 const express=require("express");
-const app =express();
+const app = express()
 const bodyparser = require("body-parser")
-
+const adminroutes =require('./routes/admin')
+const userRoutes =require('./routes/shope')
 
 app.use(bodyparser.urlencoded({extended:true}))
 
-app.get('/',(req,res)=>{
-    res.send("product home page")
-})
+app.use(adminroutes)
+app.use(userRoutes)
 
-app.get('/add-product',(req,res)=>{
-    res.send('<form action="/add-product" method= "POST" ><input type="text" name="productname" ><button type="submit">Add Product</button></form>')
-})
 
-app.post('/add-product',(req,res)=>{
-    console.log(req.body)
-    res.redirect('/')
-})
 
-app.listen(3000,()=>{console.log("server started....")}
-)
+app.listen(3000,()=>{console.log("server started....")})
--- routes/admin.js
+++ routes/admin.js
@@ -1,1 +1,15 @@
+const express =require('express');
+const  router = express.Router()
 
+
+
+router.get('/add-product',(req,res)=>{
+    res.send('<form action="/add-product" method= "POST" ><input type="text" name="productname" ><button type="submit">Add Product</button></form>')
+})
+
+router.post('/add-product',(req,res)=>{
+    console.log(req.body)
+    res.redirect('/')
+})
+
+module.exports=router
