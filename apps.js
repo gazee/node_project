b04bce7be89228c30afadfd86949eb0cd5b1c402
gazeeb <gazeebp007@gmail.com>
2022-02-12 13:40:38 +0530

    mid-ware added

--- apps.js
+++ apps.js
@@ -3,12 +3,21 @@ const app = express()
 const bodyparser = require("body-parser")
 const adminroutes =require('./routes/admin')
 const userRoutes =require('./routes/shope')
+const path =require('path')
 
 app.use(bodyparser.urlencoded({extended:true}))
 
+app.use('/admin',adminroutes)  
+// admin ennu eyuthathe thanne eni
 app.use(adminroutes)
 app.use(userRoutes)
+//eroor handiling page 
+//its like fanaling
 
+app.use((req,res,next)=>{
+    // res.status(404).send("page not found")
+    res.status(404).sendFile(path.join(__dirname,'/views/e404page.html'))
+})
 
 
 app.listen(3000,()=>{console.log("server started....")})
--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,22 @@
 const express =require('express');
 const  router = express.Router()
+const path =require('path')
 
 
 
 router.get('/add-product',(req,res)=>{
-    res.send('<form action="/add-product" method= "POST" ><input type="text" name="productname" ><button type="submit">Add Product</button></form>')
+    res.sendFile(path.join(__dirname,'..','views','ad-product.html')); 
+     //'/views/ad-product.html. enthu kondu engane ayuthunnnu bcoz of os 
 })
+// router.get('/shop',(req,res)=>{
+//     res.sendFile(path.join(__dirname,'.  .','/views/shop.html')); 
+     
+// })
 
 router.post('/add-product',(req,res)=>{
     console.log(req.body)
     res.redirect('/')
 })
+
 
 module.exports=router
